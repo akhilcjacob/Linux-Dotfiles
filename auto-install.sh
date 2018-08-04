18949cf2e402d1ba9591d6c1bace08b27525504e
akhilcjacob <akhilcjacob@gmail.com>
2018-08-03 20:09:36 -0400

    Added all files and fixed spacing issues

--- auto-install.sh
+++ auto-install.sh
@@ -3,25 +3,50 @@
 
 
 # This is for the zsh setup ( which includes a zsh setup with automatic highlighting and fish like suggestions)
+echo "Installing ZSH files"
+echo "======================"
 echo "This will override any existing zsh directories. Please be sure this is what you want!!"
-read -p "Are you sure? " -n 1 -r
+read -p "Are you sure? (y/n)" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
 cp .zshrc ~/
-
 echo "Making a plugin directory within the ~/.zsh folder..."
 mkdir ~/.zsh/plugins
-
 
 echo "Copying over syntax highlight and autosuggestion plugin..."
 cp -r zsh-plugins/zsh-syntax-highlighting ~/.zsh/plugins
 cp -r zsh-plugins/zsh-autosuggestions ~/.zsh/plugins/
 
+
+
+echo ""
 # For user verification
 echo "Plugin result ( Please make sure that zsh-syntax-highlighting and zsh-autosuggestions exist)"
 ls ~/.zsh/plugins
+fi
 
 
+echo ""
+echo ""
+# This is for the ROFI setup ( which is a dark themed app launcher )
+echo "Installing ROFI Files"
+echo "======================"
+echo "This will override any existing zsh directories. Please be sure this is what you want!!"
+read -p "Are you sure? (y/n) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+mv rofi ~/.config/rofi
+fi
 
+echo ""
+echo ""
+# This is for the i3 setup ( Setup for i3 wm - please look at .config/i3/config for keybindings)
+echo "Installing i3 wm Files"
+echo "======================"
+echo "This will override any existing zsh directories. Please be sure this is what you want!!"
+read -p "Are you sure? (y/n) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+mv i3 .config
 fi
