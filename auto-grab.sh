8e04002b029a002a9f57ff02e566d3f4f0dab132
akhilcjacob <akhilcjacob@gmail.com>
2018-08-03 20:19:16 -0400

    Updated files for kde plasma and i3 combination

--- auto-grab.sh
+++ auto-grab.sh
@@ -2,5 +2,4 @@ rm -rf compton.conf i3 rofi .zshrc userChrome.css
 cp -r ~/.config/rofi .
 cp -r ~/.config/i3 .
 cp ~/.zshrc .
-cp ~/.mozilla/firefox/j4h29iyx.default/chrome/userChrome.css .
 cp -r ~/.zsh/plugins .
